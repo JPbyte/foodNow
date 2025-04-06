56fc8081625f2e9e02e80f38cb6f8a20f62fdc64
João Pedro <pedro.fernandes1058@gmail.com>
2025-04-05 23:47:14 -0300

    feat: service listarPedidos

---
+++ foodNow/src/main/java/com/example/foodNow/service/PedidoService.java
@@ -0,0 +1,41 @@
+package com.example.foodNow.service;
+
+import com.example.foodNow.model.Pedido;
+import com.example.foodNow.model.PedidoStatus;
+import com.example.foodNow.repository.PedidoRepository;
+import com.example.foodNow.repository.ProdutoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PedidoService {
+
+    private final PedidoRepository pedidoRepository;
+    private final ProdutoRepository produtoRepository;
+
+    public Pedido criarPedido(Pedido pedido){
+
+        /* Todo pedido começa com o status recebido */
+
+        pedido.setStatus(PedidoStatus.RECEBIDO);
+        pedido.setDataTime(LocalDateTime.now());
+
+        return pedidoRepository.save(pedido);
+    }
+
+    public List<Pedido> listarPedidos(PedidoStatus status){
+        /* Caso o status não for nulo, ele filtra os pedidos
+         * Se for nulo ele retorna todos os pedidos
+        */
+
+        if(status != null){
+            return pedidoRepository.findByStatus(status);
+        } else {
+            return pedidoRepository.findAll();
+        }
+    }
+}
